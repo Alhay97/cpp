7f835931d5fae90d0a5ab389f8975bc07ace40e5
Alhai Alhameli <alhay.7@gmail.com>
2022-12-17 20:40:06 +0400

    continue

---
+++ try/basicIO.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+// endl -> is used for new line
+
+// << -> for output
+// >> -> for input
+
+
+int main()
+{
+	int num1 = 70;
+	double num2, num3;
+	char ch = 'A';
+
+	cout << "The integer: " <<num1 << endl;
+	cin >> num2 >> num3;
+	cout << "The Float: " << num2 << endl;
+	cout << "The second Float: " << num3 << endl;
+	cout << "Charecter: " << ch << endl;
+	return 0;
+}
+
