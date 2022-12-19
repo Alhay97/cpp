7b4faed367a5fd7fab200290d27519f363e7cfff
Alhai Alhameli <alhay.7@gmail.com>
2022-12-19 20:36:09 +0400

    access modifiers

--- try/objects.cpp
+++ try/objects.cpp
@@ -3,10 +3,15 @@
 class Room
 {
 	public :
+
 		double len;
 		double width;
 		double hieght;
+		Room() {
+			len = 0;
 
+		}
+		~Room();
 		double CalculateArea()
 		{
 			return len * width;
@@ -15,12 +20,17 @@ class Room
 		{
 			return len * width * hieght;
 		}
+
+		//to change a variable content that is found under private getter and setters
+		// void	setHieght(double h){
+		// 	hieght = h;
+		// }
+
 };
 
 int main()
 {
 	Room room1;
-
 	room1.hieght = 42.32;
 	room1.len = 231.32;
 	room1.width = 21.2;
@@ -28,5 +38,4 @@ int main()
 	std::cout << "the Calculated Area is: " << room1.CalculateArea() << std::endl;
 	std::cout << "the Calculated Volume is: " << room1.CalculateVolume() << std::endl;
 	return 0;
-
 }
