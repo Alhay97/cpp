d7294f0a895c4345865b9103125ede33480d6b63
Alhai Alhameli <alhay.7@gmail.com>
2022-12-25 21:30:47 +0400

    start contact

--- 42/Module00/ex01/PhoneBook.cpp
+++ 42/Module00/ex01/PhoneBook.cpp
@@ -1,12 +1,46 @@
 #include <iostream>
+#include "phonebook.hpp"
 
-class Contact
+Phonebook::Phonebook(){
+}
+Phonebook::~Phonebook(){
+}
+
+void Phonebook::set_fname(std::string fname)
 {
-	private:
-		std::string first_name;
-		std::string last_name;
-		std::string nickname;
-		std::string phonenumber;
-		std::string darkest_secret;
-};
+	this->first_name = fname;
+}
+void Phonebook::set_lname(std::string lname)
+{
+	this->last_name = lname;
+}
+void Phonebook::set_phone(std::string phone)
+{
+	this->phone_num = phone;
+}
+void Phonebook::set_insta(std::string insta)
+{
+	this->instagram = insta;
+}
+void Phonebook::set_secret(std::string sec)
+{
+	this->dark_secret = sec;
+}
 
+std::string Phonebook::get_fname()
+{
+	return (first_name);
+}
+std::string Phonebook::get_lname()
+{
+	return (last_name);
+}
+std::string Phonebook::get_num(){
+	return (phone_num);
+}
+std::string Phonebook::get_insta(){
+	return (instagram);
+}
+std::string Phonebook::get_sec(){
+	return (dark_secret);
+}
