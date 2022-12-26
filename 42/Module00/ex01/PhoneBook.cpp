dd33d433e12938a8bf287bc3cb254eb2b7c14d29
Alhay Alhamely <alhayalhamely@Alhays-MacBook-Air.local>
2022-12-26 10:16:48 +0400

    goto printing

--- 42/Module00/ex01/PhoneBook.cpp
+++ 42/Module00/ex01/PhoneBook.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 #include "phonebook.hpp"
 
+void Phonebook::infinte_getline()
+{
+    std::string ab;
+    int i = 0;
+    while (1)
+    {
+        getline(std::cin, ab);
+        if (ab == "EXIT")
+            break;
+        else if (ab == "ADD")
+        {
+            std::string tmp = "";
+            getline(std::cin, tmp);
+            person[i].set_fname(tmp);
+            getline(std::cin, tmp);
+            person[i].set_lname(tmp);
+            getline(std::cin, tmp);
+            person[i].set_phone(tmp);
+            getline(std::cin, tmp);
+            person[i].set_insta(tmp);
+            getline(std::cin, tmp);
+            person[i].set_secret(tmp);
+            i++;
+        }
+        else if (ab == "SEARCH")
+        {
+
+        }
+    }
+}
+
+void print_table(void)
+{
+    std::cout << "_____________________________________________" << std::endl;
+    std::cout << "|index     |first name|last name |insta     |" << std::endl;
+    std::cout << "|__________|__________|__________|__________|" << std::endl;
+
+}
+
+void 
+
 int main()
 {
-
 }
--- 42/Module00/ex01/phonebook.hpp
+++ 42/Module00/ex01/phonebook.hpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "contact.hpp"
 
 class Phonebook
 {
+    private:
+        Contact person[8];
+    
+    public:
+        void infinte_getline();
 
 };
