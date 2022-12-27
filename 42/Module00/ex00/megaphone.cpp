c0952bc03e1b7961af880da38c1137bb01513d7f
Alhai Alhameli <alhay.7@gmail.com>
2022-12-27 14:59:16 +0400

    continue

--- 42/Module00/ex00/megaphone.cpp
+++ 42/Module00/ex00/megaphone.cpp
@@ -6,16 +6,17 @@ int main(int counter, char **var)
 {
 	if (counter > 1)
 	{
-		for (size_t i = 1; i < counter; i++)
+		for (int i = 1; i < counter; i++)
 		{
-			size_t k = strlen(var[i]);
-			for (size_t j = 0; j < k ; j++)
+			int k = strlen(var[i]);
+			for (int j = 0; j < k ; j++)
 			{
 				if (islower(var[i][j]) != 0)
 					std::cout << (char)toupper(var[i][j]);
 				else
 					std::cout << var[i][j];
 			}
+			std::cout << " ";
 		}
 		std::cout << std::endl;
 	}
