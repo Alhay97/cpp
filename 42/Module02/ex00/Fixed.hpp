d0071cda73dca5dfdb4402e246455275483b4e64
Alhay97 <75476703+Alhay97@users.noreply.github.com>
2023-01-02 20:15:08 +0400

    Add files via upload

---
+++ 42/Module02/ex00/Fixed.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Fixed
+{
+    private:
+        int fixd_point;
+        static const int frac_bit = 8;
+
+    public:
+        Fixed();
+		Fixed(Fixed &fix);
+		Fixed& operator=(Fixed &fix);
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+
+
+
+};
