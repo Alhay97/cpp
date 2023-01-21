652dfb78e6471369d07336b9be33ec88492d79ee
Alhay97 <Alhay.7@gmail.com>
2023-01-21 18:23:14 +0400

    done with Module 06

--- Module06/ex00/main.cpp
+++ Module06/ex00/main.cpp
@@ -4,9 +4,11 @@ int main(int ac, char **av)
 {
 
 	if (ac != 2)
-		std::cout << "Error: invalid argument\n" << std::endl;
-
-	Literals conv(av[1]);
-	conv.printter();
+		std::cout << "Error: invalid argument" << std::endl;
+	else 
+	{
+		Literals conv(av[1]);
+		conv.printter();
+	}
 	return (0);
 }
