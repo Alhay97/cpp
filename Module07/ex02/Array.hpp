3fdc1a3c7fb0564929d1b65acb2d51be00048b2a
Alhay97 <Alhay.7@gmail.com>
2023-01-22 18:26:05 +0400

    fix errors

--- Module07/ex02/Array.hpp
+++ Module07/ex02/Array.hpp
@@ -3,6 +3,60 @@
 
 #include <iostream>
 
+template <class T> class Array
+{
+	private:
+		T *array;
+		int Tsize;
 
+	public:
+		Array()
+		{
+			this->array = NULL;
+			this->Tsize = 0;
+		}
+
+		Array(unsigned int _size)
+		{
+			this-> Tsize = _size;
+			this-> array = new T[_size];
+		}
+
+		Array (Array &copy)
+		{
+			*this = copy;
+		}
+
+		Array operator=(Array &source)
+		{
+			this->Tsize = source.size();
+			this->array = new T[this->_size];
+			int i = 0;
+			while( i < this->Tsize)
+			{
+				this->array[i]  = source.array[i];
+				i++;
+			}
+			return *this;
+		}
+
+		Array &operator [] (const int i ) const
+		{
+			if (i >= this->Tsize || i < 0)
+				throw std::overflow_error("index is out of bounds");
+			return this->array[i];
+		}
+
+		~Array()
+		{
+			if(this->Tsize > 0)
+				delete [] this->array;
+		}
+
+		int Size() const
+		{
+			return (this->Tsize);
+		}
+};
 
 #endif
--- Module07/ex02/main.cpp
+++ Module07/ex02/main.cpp
@@ -1,6 +1,40 @@
 #include "Array.hpp"
 
-in main()
+int main( void )
 {
-	
+	Array<int>			intArray;
+	Array<std::string>	stringArray(10);
+	Array<std::string>	otherStringArray;
+
+	stringArray[0] = "Green";
+	stringArray[1] = "Blue";
+	stringArray[2] = "Pastel";
+	stringArray[3] = "Patel";
+	stringArray[4] = "White";
+	stringArray[5] = "Green";
+	std::cout << "intArray.size() = " << intArray.size() << std::endl;
+	std::cout << "stringArray.size() = " << stringArray.size() << std::endl;
+	otherStringArray = stringArray;
+	otherStringArray[3] = "Burger";
+	std::cout << stringArray[3] << std::endl;
+	std::cout << otherStringArray[3] << std::endl;
+	try
+	{
+		std::cout << stringArray[59] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << stringArray[-3] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return 0;
 }
