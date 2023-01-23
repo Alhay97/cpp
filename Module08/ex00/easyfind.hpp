04ef361dd04a823fc00c7668797f4b4cab265dcb
Alhay97 <Alhay.7@gmail.com>
2023-01-23 18:00:19 +0400

    finish project

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -1,5 +1,17 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+#include <algorithm>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value) {
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    if (it != container.end()) {
+        return it;
+    } else {
+        throw std::invalid_argument("Value not found in container.");
+    }
+}
 
 #endif
